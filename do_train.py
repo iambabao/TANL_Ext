cc6e6440eaeabd7b1e1e8fe95a593af3db3a19b3
Qiaoben Bao <mbaoqiaoben@outlook.com>
2021-07-25 17:46:36 +0800

    update

--- do_train.py
+++ do_train.py
@@ -346,7 +346,6 @@ def main():
         "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
     )
     parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-
     parser.add_argument("--local_rank", type=int, default=-1, help="local_rank for distributed training on gpus")
     parser.add_argument(
         "--fp16",
